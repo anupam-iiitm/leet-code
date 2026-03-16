52f231e635554818e197fb99b9d97344082cac54
Anupam Shukla <sumitshukla.d.1108@gmail.com>
2026-03-16 15:16:33 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (11.85%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m=1;
+        if(n==0) return 1;
+        int i=2;
+        while(m<=n){
+            if(m==n) return m-1;
+            m=m*i;
+            
+        }
+        m=m-1;
+        return (m-n);
+    }
+};
